7ef7256fce5fe309a99d23cb6efda7dd4c86f62e
Tommy McGahee <tommy.mcgahee@usg.edu>
2018-07-24 19:52:35 -0400

    testing with docker

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-echo 'deploy script!'
+echo 'connecting to remote'
+
+openssl aes-256-cbc -K $encrypted_0d83e5d68220_key -iv $encrypted_0d83e5d68220_iv -in deploy_rsa.enc -out deploy_rsa -d
+rm deploy_rsa.enc
+eval "$(ssh-agent -s)"
+chmod 600 deploy_rsa
+ssh-add deploy_rsa
+echo $PWD
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,1 @@
 #!/bin/bash
-openssl aes-256-cbc -K $encrypted_0d83e5d68220_key -iv $encrypted_0d83e5d68220_iv -in deploy_rsa.enc -out deploy_rsa -d
-eval "$(ssh-agent -s)"
-chmod 600 deploy_rsa
-ssh-add deploy_rsa
-echo $PWD
