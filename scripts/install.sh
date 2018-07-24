fe16b6b106096e27c19390ded9d5c9d48df94e9c
Tommy McGahee <tommy.mcgahee@usg.edu>
2018-07-23 22:20:36 -0400

    more testing

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-set -x
-
 openssl aes-256-cbc -K $encrypted_0d83e5d68220_key -iv $encrypted_0d83e5d68220_iv -in deploy_rsa.enc -out deploy_rsa -d
 eval "$(ssh-agent -s)"
 chmod 600 deploy_rsa
