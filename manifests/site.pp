f1704e86bafb82da4cde41766fa35a66c1cd8b8f
Tommy <tommy.mcgahee@gmail.com>
2018-07-28 15:25:05 -0400

    updates

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,2 +1,5 @@
-include ntp
-include apache
+node default {
+  class { 'ntp':
+    servers => [ '0.north-america.pool.ntp.org', '1.north-america.pool.ntp.org', '2.north-america.pool.ntp.org', '3.north-america.pool.ntp.org'],
+  }
+}
