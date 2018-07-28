127e6787d2f9cfa6065e48e99e9e997e2821b173
Tommy <tommy.mcgahee@gmail.com>
2018-07-28 19:53:26 -0400

    updates

--- manifests/site.pp
+++ manifests/site.pp
@@ -7,6 +7,11 @@ node default {
     default_vhost => false
   }
 
+  file {'/etc/httpd/conf/mime.types':
+    ensure => link,
+    target => '/etc/mime.types',
+  }
+
   $myApacheVhosts = lookup('apache::vhosts', {})
   create_resources('apache::vhost', $myApacheVhosts)
 }
