62242610a99a42e0b237944a3b6672c90e637630
Tommy <tommy.mcgahee@gmail.com>
2018-07-28 17:17:36 -0400

    updates

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,14 @@
 node default {
   class { '::chrony':
     servers => [ '0.north-america.pool.ntp.org','1.north-america.pool.ntp.org','2.north-america.pool.ntp.org','3.north-america.pool.ntp.org'],
+  }
+
+  service { 'chronyd':
     enable => true
   }
 
   class { 'apache':
     default_vhost => false,
+    # vhosts defined in %{environment}/data/common.yaml
   }
 }
