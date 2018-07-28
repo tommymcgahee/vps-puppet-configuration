3ec10e775731341cefa533e70763755bce854d50
Tommy <tommy.mcgahee@gmail.com>
2018-07-28 17:47:53 -0400

    updates

--- manifests/site.pp
+++ manifests/site.pp
@@ -6,5 +6,10 @@ node default {
   class { 'apache':
     default_vhost => false,
     # vhosts defined in %{environment}/data/common.yaml
+    # Create a hash from Hiera Data with the Vhosts
+    $myApacheVhosts = hiera('apache::vhost', {})
+
+    # With Create Resource Converts a hash into a set of resources
+    create_resources('apache::vhost', $myApacheVhosts)
   }
 }
