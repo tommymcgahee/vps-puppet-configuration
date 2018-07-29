4d460d12a25a5801e46708bcfe92619cca4524a0
Tommy <tommy.mcgahee@gmail.com>
2018-07-28 23:57:23 -0400

    updates

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,9 +4,11 @@ node default {
   }
 
   class { 'apache':
-    default_vhost => false
+    default_vhost => false,
+    logroot_owner: apache,
+    logroot_group: apache
   }
-  
+
   $myApacheVhosts = lookup('apache::vhosts', {})
   create_resources('apache::vhost', $myApacheVhosts)
 }
