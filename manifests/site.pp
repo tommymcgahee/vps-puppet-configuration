6c252236477cb42e31aea72e81c35dfccad01ec2
Tommy <tommy.mcgahee@gmail.com>
2018-07-29 00:14:09 -0400

    updates

--- manifests/site.pp
+++ manifests/site.pp
@@ -9,4 +9,28 @@ node default {
 
   $myApacheVhosts = lookup('apache::vhosts', {})
   create_resources('apache::vhost', $myApacheVhosts)
+
+  file { '/var/log/httpd':
+    ensure => 'directory',
+    owner  => 'apache',
+    group  => 'apache'
+  }
+
+  file { '/var/log/httpd/access_log':
+    ensure => 'file',
+    owner  => 'apache',
+    group  => 'apache'
+  }
+
+  file { '/var/log/httpd/default_error.log':
+    ensure => 'file',
+    owner  => 'apache',
+    group  => 'apache'
+  }
+
+  file { '/var/log/httpd/error_log.log':
+    ensure => 'file',
+    owner  => 'apache',
+    group  => 'apache'
+  }
 }
