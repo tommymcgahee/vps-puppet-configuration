820f4fb9e267b721eb0327207798bd0cfec6fd0c
Tommy <tommy.mcgahee@gmail.com>
2018-07-29 20:39:46 -0400

    updates

--- manifests/modules/profile/manifests/sinatra.pp
+++ manifests/modules/profile/manifests/sinatra.pp
@@ -1,10 +1,10 @@
 class profile::sinatra {
 
-
   exec { 'yum groupinstall Development Tools':
     command => '/usr/bin/yum -y --disableexcludes=all groupinstall "Development Tools"',
     unless  => '/usr/bin/yum grouplist "Development Tools" | /bin/grep "^Installed"',
     timeout => 600 ,
+    before  => Package['thin']
   }
 
   package { 'sinatra':
@@ -17,7 +17,6 @@ class profile::sinatra {
     provider => 'gem',
     alias    => 'ruby-thin',
     ensure   => 'installed',
-    require  => Package['ruby-devel']
   }
 
 }
