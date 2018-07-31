c8fe7365217818e24c31d7ebab4a42dbca61e8b1
Tommy McGahee <tommy.mcgahee@usg.edu>
2018-07-31 16:12:27 -0400

    testing permissions

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -8,14 +8,14 @@ set -e
 "$@"
 
 # Pull from control repo and create dynamic environments
-sudo /opt/puppetlabs/puppet/bin/r10k deploy environment -p --config /r10k.yaml
+/opt/puppetlabs/puppet/bin/r10k deploy environment -p --config /r10k.yaml
 
 # r10k does NOT do module dependency resolution, so we need Puppet to check for that
 tput setaf 2; echo "Checking module dependencies"
-sudo /opt/puppetlabs/bin/puppet module list --tree
+/opt/puppetlabs/bin/puppet module list --tree
 
 # Compile and apply catalog(s)
-sudo /opt/puppetlabs/bin/puppet apply
+/opt/puppetlabs/bin/puppet apply
 
 if [[ "$DEV_MODE" = "true" ]]; then
 	echo "Sleeping to keep container running for local shenanigans."
